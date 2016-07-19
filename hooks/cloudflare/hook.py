789de4cba61772a3327d7f56939158738918c911
OWS Deploy <deploy@ows.vn>
2016-07-19 10:43:59 +0700

    fixed cloudflare error

--- hooks/cloudflare/hook.py
+++ hooks/cloudflare/hook.py
@@ -124,9 +124,14 @@ def delete_txt_record(args):
 
 
 def deploy_cert(args):
-    domain, privkey_pem, cert_pem, fullchain_pem, chain_pem = args
+    domain, privkey_pem, cert_pem, fullchain_pem, chain_pem, datetime = args
     logger.info(' + ssl_certificate: {0}'.format(fullchain_pem))
     logger.info(' + ssl_certificate_key: {0}'.format(privkey_pem))
+    return
+
+def unchanged_cert(args):
+    domain, privkey_pem, cert_pem, fullchain_pem, chain_pem = args
+    logger.info(' + unchanged_cert for {0}'.format(domain))
     return
 
 
@@ -135,8 +140,9 @@ def main(argv):
         'deploy_challenge': create_txt_record,
         'clean_challenge' : delete_txt_record,
         'deploy_cert'     : deploy_cert,
+	'unchanged_cert'  : unchanged_cert,
     }
-    logger.info(" + CloudFlare hook executing: {0}".format(argv[0]))
+    logger.info(" + CloudFlare hook executing: {0} : {1}".format(argv[0], argv[1:]))
     ops[argv[0]](argv[1:])
 
 
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ export CF_EMAIL='trongpd@ows.vn'
 export CF_KEY='8c2520e99433e55bd2133d9762c4869adcad3'
 echo "\n\nChecking for new ssl $(date +'%m/%d/%Y %T')\n" >> /var/log/autossl.log
 cd /opt/autossl/letsencrypt.sh
-./letsencrypt.sh -c >> /var/log/autossl.log
+./letsencrypt.sh -c --config ./config.sh --hook ./hook.sh # >> /var/log/autossl.log
 chmod -R 755 /opt/autossl/letsencrypt.sh/certs
